6002477218dbc0d5bb02e7c17e9c3a10ffe94ffe
webmaster978 <jtjoel11@gmail.com>
2021-04-04 03:47:56 +0200

    profile vrai

--- pro.php
+++ pro.php
@@ -188,7 +188,7 @@ WHERE teacher_id = '" . $_SESSION["teacher_id"] . "'
                     <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i><?= $s->teacher_qualification; ?></p>
                     <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i><?= $s->teacher_address; ?></p>
                     <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i><?= $s->teacher_emailid; ?></p>
-                    <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>1224435534</p>
+
                     <hr>
                 <?php
                     endforeach;
@@ -201,51 +201,79 @@ WHERE teacher_id = '" . $_SESSION["teacher_id"] . "'
 
             <!-- End Left Column -->
         </div>
-
+        <span><?php echo $success; ?></span>
         <!-- Right Column -->
         <div class="w3-twothird">
 
             <div class="w3-container w3-card w3-white w3-margin-bottom">
-                <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
-                <div class="w3-container">
-                    <h5 class="w3-opacity"><b>Front End Developer / </b></h5>
-                    <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
-                    <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-                    <hr>
-                </div>
-                <div class="w3-container">
-                    <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-                    <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-                    <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-                    <hr>
-                </div>
-                <div class="w3-container">
-                    <h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-                    <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
-                </div>
+                <form method="post" id="profile_form" enctype="multipart/form-data">
+
+                    <div class="w3-container">
+                        <label class="col-md-4 text-right">Nom du prof <span class="text-danger">*</span></label>
+                        <div class="col-md-12">
+                            <input type="text" name="teacher_name" id="teacher_name" class="form-control" />
+                            <span class="text-danger"><?php echo $error_teacher_name; ?></span>
+                        </div>
+
+                    </div>
+                    <div class="w3-container">
+                        <label class="col-md-4 text-right">Address <span class="text-danger">*</span></label>
+                        <div class="col-md-12">
+                            <textarea name="teacher_address" id="teacher_address" class="form-control"></textarea>
+                            <span class="text-danger"><?php echo $error_teacher_address; ?></span>
+                        </div>
+
+                    </div>
+                    <div class="w3-container">
+                        <label class="col-md-4 text-right">Address mail <span class="text-danger">*</span></label>
+                        <div class="col-md-12">
+                            <input type="text" name="teacher_emailid" id="teacher_emailid" class="form-control" />
+                            <span class="text-danger"><?php echo $error_teacher_emailid; ?></span>
+                        </div>
+                    </div>
+                    <div class="w3-container">
+                        <label class="col-md-4 text-right">Qualification <span class="text-danger">*</span></label>
+                        <div class="col-md-12">
+                            <input type="text" name="teacher_qualification" id="teacher_qualification" class="form-control" />
+                            <span class="text-danger"><?php echo $error_teacher_qualification; ?></span>
+                        </div>
+                    </div>
+                    <div class="w3-container">
+                        <label class="col-md-4 text-right">Grade <span class="text-danger">*</span></label>
+                        <div class="col-md-8">
+                            <select name="teacher_grade_id" id="teacher_grade_id" class="form-control">
+                                <option value="">Selectionner le Grade</option>
+                                <?php
+                                echo load_grade_list($connect);
+                                ?>
+                            </select>
+                            <span class="text-danger"><?php echo $error_teacher_grade_id; ?></span>
+                        </div>
+                    </div>
+                    <div class="w3-container">
+                        <label class="col-md-4 text-right">Date d''engagement <span class="text-danger">*</span></label>
+                        <div class="col-md-8">
+                            <input type="text" name="teacher_doj" id="teacher_doj" class="form-control" readonly />
+                            <span class="text-danger"><?php echo $error_teacher_doj; ?></span>
+                        </div>
+                    </div>
+                    <div class="w3-container">
+                        <label class="col-md-4 text-right">Image <span class="text-danger">*</span></label>
+                        <div class="col-md-8">
+                            <input type="file" name="teacher_image" id="teacher_image" />
+                            <span class="text-muted">Seulement .jpg et .png </span><br />
+                            <span id="error_teacher_image" class="text-danger"><?php echo $error_teacher_image; ?></span>
+                        </div>
+                    </div>
+                    <div class="card-footer" align="center">
+                        <input type="hidden" name="hidden_teacher_image" id="hidden_teacher_image" />
+                        <input type="hidden" name="teacher_id" id="teacher_id" />
+                        <input type="submit" name="button_action" id="button_action" class="btn btn-success btn-sm" value="Modifier" />
+                    </div>
+                </form>
             </div>
 
-            <div class="w3-container w3-card w3-white">
-                <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
-                <div class="w3-container">
-                    <h5 class="w3-opacity"><b>W3Schools.com</b></h5>
-                    <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
-                    <p>Web Development! All I need to know in one place</p>
-                    <hr>
-                </div>
-                <div class="w3-container">
-                    <h5 class="w3-opacity"><b>London Business School</b></h5>
-                    <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
-                    <p>Master Degree</p>
-                    <hr>
-                </div>
-                <div class="w3-container">
-                    <h5 class="w3-opacity"><b>School of Coding</b></h5>
-                    <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
-                    <p>Bachelor Degree</p><br>
-                </div>
-            </div>
+
 
             <!-- End Right Column -->
         </div>
--- profile.php
+++ profile.php
@@ -93,15 +93,15 @@ if (isset($_POST["button_action"])) {
 	if ($error == 0) {
 		if ($teacher_password != '') {
 			$data = array(
-				':teacher_name'			=>	$teacher_name,
-				':teacher_address'		=>	$teacher_address,
-				':teacher_emailid'		=>	$teacher_emailid,
-				':teacher_password'		=>	password_hash($teacher_password, PASSWORD_DEFAULT),
-				':teacher_qualification' =>	$teacher_qualification,
-				':teacher_doj'			=>	$teacher_doj,
-				':teacher_image'		=>	$teacher_image,
-				':teacher_grade_id'		=>	$teacher_grade_id,
-				':teacher_id'			=>	$_POST["teacher_id"]
+				':teacher_name'            =>    $teacher_name,
+				':teacher_address'        =>    $teacher_address,
+				':teacher_emailid'        =>    $teacher_emailid,
+				':teacher_password'        =>    password_hash($teacher_password, PASSWORD_DEFAULT),
+				':teacher_qualification' =>    $teacher_qualification,
+				':teacher_doj'            =>    $teacher_doj,
+				':teacher_image'        =>    $teacher_image,
+				':teacher_grade_id'        =>    $teacher_grade_id,
+				':teacher_id'            =>    $_POST["teacher_id"]
 			);
 			$query = "
 			UPDATE tbl_teacher 
@@ -117,14 +117,14 @@ if (isset($_POST["button_action"])) {
 			";
 		} else {
 			$data = array(
-				':teacher_name'			=>	$teacher_name,
-				':teacher_address'		=>	$teacher_address,
-				':teacher_emailid'		=>	$teacher_emailid,
-				':teacher_qualification' =>	$teacher_qualification,
-				':teacher_doj'			=>	$teacher_doj,
-				':teacher_image'		=>	$teacher_image,
-				':teacher_grade_id'		=>	$teacher_grade_id,
-				':teacher_id'			=>	$_POST["teacher_id"]
+				':teacher_name'            =>    $teacher_name,
+				':teacher_address'        =>    $teacher_address,
+				':teacher_emailid'        =>    $teacher_emailid,
+				':teacher_qualification' =>    $teacher_qualification,
+				':teacher_doj'            =>    $teacher_doj,
+				':teacher_image'        =>    $teacher_image,
+				':teacher_grade_id'        =>    $teacher_grade_id,
+				':teacher_id'            =>    $_POST["teacher_id"]
 			);
 			$query = "
 			UPDATE tbl_teacher 
@@ -158,65 +158,87 @@ $result = $statement->fetchAll();
 
 ?>
 
-<div class="container" style="margin-top:30px">
-	<span><?php echo $success; ?></span>
-	<div class="card">
-		<form method="post" id="profile_form" enctype="multipart/form-data">
-			<div class="card-header">
-				<div class="row">
-					<div class="col-md-9">Profile</div>
-					<div class="col-md-3" align="right">
-					</div>
+
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+	<!-- The Grid -->
+	<div class="w3-row-padding">
+
+		<!-- Left Column -->
+		<div class="w3-third">
+
+			<div class="w3-white w3-text-grey w3-card-4">
+				<div class="w3-display-container">
+					<?php
+
+					$service = $connect->prepare("
+SELECT * FROM tbl_teacher 
+WHERE teacher_id = '" . $_SESSION["teacher_id"] . "'
+");
+					$service->execute();
+					$don = $service->fetchAll(PDO::FETCH_OBJ);
+					foreach ($don as $s) :
+					?>
+						<img src="admin/teacher_image/<?= $s->teacher_image; ?>" class="img-thumbnail" width="300">
+						<div class="w3-display-bottomleft w3-container w3-text-black">
+							<h2><?= $s->teacher_name; ?></h2>
+						</div>
 				</div>
-			</div>
-			<div class="card-body">
-				<div class="form-group">
-					<div class="row">
+				<div class="w3-container">
+					<p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i><?= $s->teacher_qualification; ?></p>
+					<p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i><?= $s->teacher_address; ?></p>
+					<p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i><?= $s->teacher_emailid; ?></p>
+
+					<hr>
+				<?php
+					endforeach;
+
+
+				?>
+				<br>
+				</div>
+			</div><br>
+
+			<!-- End Left Column -->
+		</div>
+		<span><?php echo $success; ?></span>
+		<!-- Right Column -->
+		<div class="w3-twothird">
+
+			<div class="w3-container w3-card w3-white w3-margin-bottom">
+				<form method="post" id="profile_form" enctype="multipart/form-data">
+
+					<div class="w3-container">
 						<label class="col-md-4 text-right">Nom du prof <span class="text-danger">*</span></label>
-						<div class="col-md-8">
+						<div class="col-md-12">
 							<input type="text" name="teacher_name" id="teacher_name" class="form-control" />
 							<span class="text-danger"><?php echo $error_teacher_name; ?></span>
 						</div>
+
 					</div>
-				</div>
-				<div class="form-group">
-					<div class="row">
+					<div class="w3-container">
 						<label class="col-md-4 text-right">Address <span class="text-danger">*</span></label>
-						<div class="col-md-8">
+						<div class="col-md-12">
 							<textarea name="teacher_address" id="teacher_address" class="form-control"></textarea>
 							<span class="text-danger"><?php echo $error_teacher_address; ?></span>
 						</div>
+
 					</div>
-				</div>
-				<div class="form-group">
-					<div class="row">
+					<div class="w3-container">
 						<label class="col-md-4 text-right">Address mail <span class="text-danger">*</span></label>
-						<div class="col-md-8">
+						<div class="col-md-12">
 							<input type="text" name="teacher_emailid" id="teacher_emailid" class="form-control" />
 							<span class="text-danger"><?php echo $error_teacher_emailid; ?></span>
 						</div>
 					</div>
-				</div>
-				<div class="form-group">
-					<div class="row">
-						<label class="col-md-4 text-right">Mot de passe <span class="text-danger">*</span></label>
-						<div class="col-md-8">
-							<input type="password" name="teacher_password" id="teacher_password" class="form-control" placeholder="Leave blank to not change it" />
-							<span class="text-danger"></span>
-						</div>
-					</div>
-				</div>
-				<div class="form-group">
-					<div class="row">
+					<div class="w3-container">
 						<label class="col-md-4 text-right">Qualification <span class="text-danger">*</span></label>
-						<div class="col-md-8">
+						<div class="col-md-12">
 							<input type="text" name="teacher_qualification" id="teacher_qualification" class="form-control" />
 							<span class="text-danger"><?php echo $error_teacher_qualification; ?></span>
 						</div>
 					</div>
-				</div>
-				<div class="form-group">
-					<div class="row">
+					<div class="w3-container">
 						<label class="col-md-4 text-right">Grade <span class="text-danger">*</span></label>
 						<div class="col-md-8">
 							<select name="teacher_grade_id" id="teacher_grade_id" class="form-control">
@@ -228,18 +250,14 @@ $result = $statement->fetchAll();
 							<span class="text-danger"><?php echo $error_teacher_grade_id; ?></span>
 						</div>
 					</div>
-				</div>
-				<div class="form-group">
-					<div class="row">
+					<div class="w3-container">
 						<label class="col-md-4 text-right">Date d''engagement <span class="text-danger">*</span></label>
 						<div class="col-md-8">
 							<input type="text" name="teacher_doj" id="teacher_doj" class="form-control" readonly />
 							<span class="text-danger"><?php echo $error_teacher_doj; ?></span>
 						</div>
 					</div>
-				</div>
-				<div class="form-group">
-					<div class="row">
+					<div class="w3-container">
 						<label class="col-md-4 text-right">Image <span class="text-danger">*</span></label>
 						<div class="col-md-8">
 							<input type="file" name="teacher_image" id="teacher_image" />
@@ -247,22 +265,27 @@ $result = $statement->fetchAll();
 							<span id="error_teacher_image" class="text-danger"><?php echo $error_teacher_image; ?></span>
 						</div>
 					</div>
-				</div>
+					<div class="card-footer" align="center">
+						<input type="hidden" name="hidden_teacher_image" id="hidden_teacher_image" />
+						<input type="hidden" name="teacher_id" id="teacher_id" />
+						<input type="submit" name="button_action" id="button_action" class="btn btn-success btn-sm" value="Modifier" />
+					</div>
+				</form>
 			</div>
-			<div class="card-footer" align="center">
-				<input type="hidden" name="hidden_teacher_image" id="hidden_teacher_image" />
-				<input type="hidden" name="teacher_id" id="teacher_id" />
-				<input type="submit" name="button_action" id="button_action" class="btn btn-success btn-sm" value="Modifier" />
-			</div>
-		</form>
+
+
+
+			<!-- End Right Column -->
+		</div>
+
+		<!-- End Grid -->
 	</div>
+
+	<!-- End Page Container -->
 </div>
-<br />
-<br />
-</body>
 
 </html>
-
+</body>
 <script type="text/javascript" src="js/bootstrap-datepicker.js"></script>
 <link rel="stylesheet" href="css/datepicker.css" />
 
